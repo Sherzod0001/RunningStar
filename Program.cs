8388c617f874540b1d11fc5ada62e3999c91bb63
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-13 10:52:25 +0500

    initialize project

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+Console.Clear();
+
+Console.WriteLine("Welcome to our Running Star program");
+
+int axisX = 1;
+int axisY = 1;
+
+Console.SetCursorPosition(axisX, axisY);
+Console.Write('*');
+
+while (true)
+{
+    if (axisX > 0 || axisY > 0)
+    {
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo keyInput = Console.ReadKey(true);
+
+            Console.SetCursorPosition(axisX, axisY);
+            Console.Write(' ');
+
+            switch (keyInput.Key)
+            {
+                case ConsoleKey.RightArrow:
+                    axisX++;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    axisX--;
+                    break;
+                case ConsoleKey.UpArrow:
+                    axisY--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    axisY++;
+                    break;
+            }
+
+            Console.SetCursorPosition(axisX, axisY);
+            Console.Write('*');
+        }
+    }
+    else 
+    {
+        Console.WriteLine("An error key was entered !!! ");
+        Console.WriteLine("Please enter only '➡️' '⬅️' '⬆️' '⬇️' ");
+    }
+    
+}
